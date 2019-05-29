8000f980ac31c577cf2501af39ae575f30ba57bf
Diego Marvid <diegomarvid99@gmail.com>
2019-05-28 23:32:12 -0300

    Particionando GUI entre display y control
    Se agrego una clase que lea la jugada desde el digital lab. Se tiene en cuenta las coordenadas de los colores por si van cambiando.
    
    Asimismo, se agregaron metodos de refresh display y display light para actualizar el display y poder mostrar la jugada y/o secuencia en el display

---
+++ GUI_control.asm
@@ -0,0 +1,180 @@
+.eqv enable 0xFFFF0012
+.eqv scanner 0xFFFF0014
+
+.globl get_play
+
+.data
+
+
+.text
+
+
+#**********LEO DIGITAL LAB*********#
+
+# $v0 -> color seleccionado
+# $v0 -> -1 | No selecciono nada en
+# su intervalo dado
+
+get_play:
+
+addiu $sp,$sp,-8
+sw $ra,($sp)
+sw $s0,4($sp)
+
+#Valor inicial -> no apreto teclas
+li $s0,-1
+
+#Mi tiempo inicial
+li $v0,30
+syscall
+move $t3,$a0
+
+get_play_loop:
+
+#Mi tiempo actual
+li $v0,30
+syscall
+move $t4,$a0
+
+#Calculo diferencia de tiempo
+subu $t4,$t4,$t3
+
+#Evaluo si estoy en menos tiempo del maximo
+bgt $t4,2000,get_play_fin
+
+#Prendo fila 1
+li $t1,1
+sb $t1,enable
+lbu $t0,scanner
+
+beq $t0, 0x11, cuadrante_1
+beq $t0, 0x21, cuadrante_1
+beq $t0, 0x41, cuadrante_2
+beq $t0, 0x81, cuadrante_2
+
+#Prendo fila 2
+li $t1,2
+sb $t1,enable
+lbu $t0,scanner
+
+beq $t0, 0x12, cuadrante_1
+beq $t0, 0x22, cuadrante_1
+beq $t0, 0x42, cuadrante_2
+beq $t0, 0x82, cuadrante_2
+
+#Prendo fila 3
+li $t1,4
+sb $t1,enable
+lbu $t0,scanner	
+
+beq $t0, 0x14, cuadrante_4
+beq $t0, 0x24, cuadrante_4
+beq $t0, 0x44, cuadrante_3
+beq $t0, 0x84, cuadrante_3
+
+#Prendo fila 4
+li $t1,8
+sb $t1,enable
+lbu $t0,scanner
+
+beq $t0, 0x18, cuadrante_4
+beq $t0, 0x28, cuadrante_4
+beq $t0, 0x48, cuadrante_3
+beq $t0, 0x88, cuadrante_3
+
+
+j get_play_loop
+
+cuadrante_1:
+
+li $a0,0
+jal cuadrante_color
+
+move $s0,$v0
+
+j get_play_fin
+
+cuadrante_2:
+
+li $a0,1
+jal cuadrante_color
+
+move $s0,$v0
+
+j get_play_fin
+
+cuadrante_3:
+
+li $a0,2
+jal cuadrante_color
+
+move $s0,$v0
+
+j get_play_fin
+
+cuadrante_4:
+
+li $a0,3
+jal cuadrante_color
+
+move $s0,$v0
+
+j get_play_fin
+
+get_play_fin:
+
+move $v0,$s0
+
+lw $ra,($sp)
+lw $s0,4($sp)
+addiu $sp,$sp,8
+
+jr $ra
+
+#**********CUADRANTE COLOR*********************#
+
+# $a0 - > Cuadrante seleccionado
+
+cuadrante_color:
+
+li $v0,-1
+
+# Me fijo a que color hace referencia el cuadrante:
+
+lw $t0,green_cord
+beq $a0,$t0,cuadrante_color_green
+
+lw $t0,red_cord
+beq $a0,$t0,cuadrante_color_red
+
+lw $t0,yellow_cord
+beq $a0,$t0,cuadrante_color_yellow
+
+lw $t0,blue_cord
+beq $a0,$t0,cuadrante_color_blue
+
+j cuadrante_color_fin
+
+cuadrante_color_green:
+li $v0,0
+j cuadrante_color_fin
+
+cuadrante_color_red:
+li $v0,1
+j cuadrante_color_fin
+
+cuadrante_color_blue:
+li $v0,2
+j cuadrante_color_fin
+
+cuadrante_color_yellow:
+li $v0,3
+j cuadrante_color_fin
+
+cuadrante_color_fin:
+
+jr $ra
+
+
+
+
---
+++ GUI_display.asm
@@ -0,0 +1,321 @@
+.eqv green  0x00FF00
+.eqv red    0xFF0000
+.eqv yellow 0xFFFF00
+.eqv blue   0x0000FF
+
+.eqv green_light  0xAAFE90
+.eqv red_light    0xF08080
+.eqv yellow_light 0xFFFF99
+.eqv blue_light   0x1E90FF
+
+.globl green_cord
+.globl red_cord
+.globl yellow_cord
+.globl blue_cord
+
+.globl refresh_coordinates
+.globl refresh_display
+.globl display_light
+
+.data
+
+#Unit width in pixels: 32
+#Unit height in pixels: 32
+#Display width in Pixels: 256
+#Display Height in pixels: 256
+#Base address: static data
+
+display: .space 256
+
+green_cord: .word 0
+red_cord: .word 1
+blue_cord: .word 2
+yellow_cord: .word 3
+
+.text
+
+
+
+#************MAPEO JUGADA A COLOR******#
+
+
+#En $a0 recibo el modo de juego
+
+
+refresh_coordinates:
+
+beq $a0,3,refresh_trickster
+
+li $t0,0
+sw $t0,green_cord
+
+li $t0,1
+sw $t0,red_cord
+
+li $t0,2
+sw $t0,blue_cord
+
+li $t0,3
+sw $t0,yellow_cord
+
+j refresh_coordinates_fin
+
+refresh_trickster:
+
+#-Esquina para verde-#
+
+#Genero RANDOM en $a0
+li $a1,4
+li $v0,42
+syscall
+
+sw $a0,green_cord
+
+move $t0,$a0
+
+#-Esquina para rojo-#
+
+refresh_trickster_red:
+
+li $a1,4
+li $v0,42
+syscall
+
+beq $a0,$t0,refresh_trickster_red
+
+
+sw $a0,red_cord
+
+move $t1,$a0
+
+#-Esquina para azul-#
+
+refresh_trickster_blue:
+
+li $a1,4
+li $v0,42
+syscall
+
+beq $a0,$t0,refresh_trickster_blue
+beq $a0,$t1,refresh_trickster_blue
+
+sw $a0,blue_cord
+
+move $t2,$a0
+
+#-Esquina para amarillo-#
+
+refresh_trickster_yellow:
+
+li $a1,4
+li $v0,42
+syscall
+
+beq $a0,$t0,refresh_trickster_yellow
+beq $a0,$t1,refresh_trickster_yellow
+beq $a0,$t2,refresh_trickster_yellow
+
+
+sw $a0,yellow_cord
+
+move $t3,$a0
+
+refresh_coordinates_fin:
+jr $ra
+
+#*****************CUADRADO**************#
+
+# $a0 -> esquina
+# $a1 -> Color
+
+cuadrado:
+
+li $t0,3 #Contador filas
+li $t1,4 #Contador columnas
+
+#Saber cuantas columnas saltear
+li $t5,16
+multu $a0,$t5
+mflo $t5
+
+
+la $t2,display #Direccion para pintar filas
+
+bgt $a0,1,cuadrado_esquinas_abajo
+
+addu $t2,$t2,$t5
+
+
+j cuadrado_columnas_loop
+
+cuadrado_esquinas_abajo:
+
+li $t4,176
+
+subu $t5,$t4,$t5
+addu $t2,$t2,$t5
+
+#Pinto columnas
+cuadrado_columnas_loop:
+
+beqz $t1,cuadrado_filas
+
+sw $a1,($t2)
+
+addiu $t2,$t2,4
+
+addiu $t1,$t1,-1
+
+j cuadrado_columnas_loop
+
+cuadrado_filas:
+
+beqz $t0,cuadrado_fin
+
+#Cambio de fila
+addiu $t2,$t2,16
+
+addiu $t0,$t0,-1
+
+#Reinicio contador columnas
+li $t1,4
+
+j cuadrado_columnas_loop
+
+
+cuadrado_fin:
+jr $ra
+
+
+#**************DISPLAY LIGHT**********#
+
+# $a0 -> Jugada
+
+display_light:
+
+addiu $sp,$sp,-8
+sw $ra,($sp)
+
+beq $a0,0,display_light_green
+
+beq $a0,1,display_light_red
+
+beq $a0,2,display_light_blue
+
+beq $a0,3,display_light_yellow
+
+
+display_light_green:
+
+lw $a0,green_cord
+li $a1,green_light
+jal cuadrado
+
+li $a0,500
+li $v0,32
+syscall
+
+lw $a0,green_cord
+li $a1,green
+jal cuadrado
+
+j display_light_fin
+
+display_light_red:
+
+lw $a0,red_cord
+li $a1,red_light
+jal cuadrado
+
+li $a0,500
+li $v0,32
+syscall
+
+
+lw $a0,red_cord
+li $a1,red
+jal cuadrado
+
+j display_light_fin
+
+display_light_blue:
+
+lw $a0,blue_cord
+li $a1,blue_light
+jal cuadrado
+
+li $a0,500
+li $v0,32
+syscall
+
+lw $a0,blue_cord
+li $a1,blue
+jal cuadrado
+
+
+j display_light_fin
+
+display_light_yellow:
+
+lw $a0,yellow_cord
+li $a1,yellow_light
+jal cuadrado
+
+li $a0,500
+li $v0,32
+syscall
+
+lw $a0,yellow_cord
+li $a1,yellow
+jal cuadrado
+
+j display_light_fin
+
+
+display_light_fin:
+
+#jal display_init
+
+lw $ra,($sp)
+addiu $sp,$sp,8
+jr $ra
+
+#**********Refresh display***********#
+
+refresh_display:
+
+addiu $sp,$sp,-4
+sw $ra,($sp)
+
+
+lw $a0,green_cord
+li $a1,green
+jal cuadrado
+
+
+lw $a0,red_cord
+li $a1,red
+jal cuadrado
+
+lw $a0,blue_cord
+li $a1,blue
+jal cuadrado
+
+lw $a0,yellow_cord
+li $a1,yellow
+jal cuadrado
+
+refresh_display_fin:
+
+
+lw $ra,($sp)
+addiu $sp,$sp,4
+jr $ra
+
+
+
+
+
+
+
+
