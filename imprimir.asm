c2f99c06a865a80e26b8988a971e677c085c8cc5
Diego Marvid <diegomarvid99@gmail.com>
2019-05-30 01:14:39 -0300

    Arreglando error de sonido
    Faltaba agregarle persistencia al registro que se encargaba de pasarle el parametro a la funcion de sonido. Se agrego un registro s3 que guarda el valor de la secuencia y este mismo se usa de parametro en sonido_secuencia.

--- imprimir.asm
+++ imprimir.asm
@@ -15,10 +15,11 @@ instruccionstr: .asciiz "Apriete 1 si quiere volver a jugar, de lo contratrio ap
 
 imprimir_secuencia:
 
-addiu $sp,$sp,-12
+addiu $sp,$sp,-16
 sw $ra,($sp)
 sw $s0,4($sp)
 sw $s2,8($sp)
+sw $s3,12($sp)
 
 la $s0, ($a1)
 move $s2, $a0 #Numero de turno, se usa para saber cuantas veces imprimir
@@ -27,16 +28,16 @@ loop:
 
 beqz $s2, imprimir_fin
 
-lb $t1,($s0)
+lb $s3,($s0) #Valor de la secuencia para mostrar
 
 
 
 #li $v0,1
-move $a0,$t1
+move $a0,$s3
 jal display_light
 #syscall
 
-move $a0,$t1
+move $a0,$s3
 jal sonido_secuencia
 
 
@@ -51,7 +52,8 @@ imprimir_fin:
 lw $ra,($sp)
 lw $s0,4($sp)
 lw $s2,8($sp)
-addiu $sp,$sp,12
+lw $s3,12($sp)
+addiu $sp,$sp,16
 jr $ra
 
 #---------------------------------------------------------------
