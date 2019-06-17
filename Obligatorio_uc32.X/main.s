ba9d44149256cc50cfba59a45590c04018a92a73
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2019-06-17 20:09:23 -0300

    Inicio uc32

---
+++ Obligatorio_uc32.X/main.s
@@ -0,0 +1,50 @@
+.globl main
+    
+.text
+    
+.ent main
+    
+    main:
+    
+    
+    
+    # Configurar puerto G como entrada #RG7 -> 12
+    li $t0, 128
+    sw $t0, TRISG
+    
+    # Configurar puerto F como salida 4 RF1
+    li $t0, 0 
+    sw $t0, TRISF
+    
+    # Configurar puerto E como salida para gnd
+    li $t0, 0 
+    sw $t0, TRISE
+    
+    sw $t0,PORTE
+    
+    
+    loop:
+    
+    # Obtengo valor del boton
+    lw $t0,PORTG
+    
+    andi $t0,$t0,128
+    
+    # Debouncing
+    
+    
+    
+    
+    
+    
+    # Cargo el led con el valor del boton
+    
+    # Paso a posicion de la 6 a la 1 para escribir en RF1 | 4
+    srl $t0,$t0,6
+    sw $t0,PORTF
+    
+    j loop
+    
+.end main
+
+
---
+++ Obligatorio_uc32.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Obligatorio_uc32.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Obligatorio_uc32.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=obligatoriouc32.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/obligatoriouc32.x.tar
+# Copy_of_default configuration
+CND_ARTIFACT_DIR_Copy_of_default=dist/Copy_of_default/production
+CND_ARTIFACT_NAME_Copy_of_default=Obligatorio_uc32.X.production.hex
+CND_ARTIFACT_PATH_Copy_of_default=dist/Copy_of_default/production/Obligatorio_uc32.X.production.hex
+CND_PACKAGE_DIR_Copy_of_default=${CND_DISTDIR}/Copy_of_default/package
+CND_PACKAGE_NAME_Copy_of_default=obligatoriouc32.x.tar
+CND_PACKAGE_PATH_Copy_of_default=${CND_DISTDIR}/Copy_of_default/package/obligatoriouc32.x.tar
